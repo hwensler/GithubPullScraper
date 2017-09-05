385c274ed96eefa96d256c249c38ee70c146d83f
HWN <heatherwensler@gmail.com>
2017-09-05 14:52:57 -0700

    removed unnecessary new lines and whatnot from body

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function closePull(url) {
             authoriation: oAuthToken
         },
         body:
-            '{\r\n"state": "closed"\r\n}'
+            '{"state": "closed"}'
     }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log("Closed pull request. ");
